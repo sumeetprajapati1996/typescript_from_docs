93f6a23acc4b1006e07eada975c4394b8af96089
Sumeet Prajapati <sumeetprajapati15@gmail.com>
2022-04-24 20:14:49 +0530

    Delete the output folder

--- vs_code_docs/HelloWorld/out/helloworld.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// vs_code_docs/HelloWorld/helloworld.ts
-// Basic hello world from microsoft vs code documentation
-let message = "Hello World!";
-// Output
-console.log(message);
-//# sourceMappingURL=helloworld.js.map
