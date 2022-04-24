90d9365bff89af29fca8234bc7d94b08f8eeb5ac
Sumeet Prajapati <sumeetprajapati15@gmail.com>
2022-04-24 20:08:08 +0530

    Implement VS Code Typescript tutorial

---
+++ vs_code_docs/HelloWorld/helloworld.ts
@@ -0,0 +1,6 @@
+// vs_code_docs/HelloWorld/helloworld.ts
+
+// Basic hello world from microsoft vs code documentation
+let message: string = "Hello World!";
+// Output
+console.log(message);
---
+++ vs_code_docs/HelloWorld/out/helloworld.js
@@ -0,0 +1,7 @@
+"use strict";
+// vs_code_docs/HelloWorld/helloworld.ts
+// Basic hello world from microsoft vs code documentation
+let message = "Hello World!";
+// Output
+console.log(message);
+//# sourceMappingURL=helloworld.js.map
