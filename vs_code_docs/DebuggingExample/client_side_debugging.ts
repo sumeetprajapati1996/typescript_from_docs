f8c3ff44d4b00f13cd48b32953f31f65cf463c87
Sumeet Prajapati <sumeetprajapati15@gmail.com>
2022-04-25 19:52:20 +0530

    Implement VS Code Debugging tutorial

---
+++ vs_code_docs/DebuggingExample/client_side_debugging.ts
@@ -0,0 +1,4 @@
+// vs_code_docs/DebuggingExample/client_side_debugging.ts
+let message: string = "Hello World";
+
+document.body.innerHTML = message;
---
+++ vs_code_docs/DebuggingExample/out/client_side_debugging.js
@@ -0,0 +1,5 @@
+"use strict";
+// vs_code_docs/DebuggingExample/client_side_debugging.ts
+var message = "Hello World";
+document.body.innerHTML = message;
+//# sourceMappingURL=client_side_debugging.js.map
