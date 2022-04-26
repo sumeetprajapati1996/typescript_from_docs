5e22736fd3ab1da097112c12c652b8ba7d415085
Sumeet Prajapati <sumeetprajapati15@gmail.com>
2022-04-26 20:33:01 +0530

    Functions in typescript

---
+++ official_docs/05_functions.ts
@@ -0,0 +1,13 @@
+/**
+ * Functions
+ */
+// Argument annotations
+function greet(name: string) {
+  console.log("Hello, " + name.toUpperCase() + "!!");
+}
+greet("Sumeet");
+
+// Return type annotations
+function getFavouriteNumber(): number {
+  return 26;
+}
