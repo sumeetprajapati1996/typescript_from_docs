c7ea14192e84714aff6dbc9be53c4e14bd20d6e4
Sumeet Prajapati <sumeetprajapati15@gmail.com>
2022-04-27 02:08:32 +0530

    Less Common Primitives in typescript

---
+++ official_docs/14_less_common_primitives.ts
@@ -0,0 +1,11 @@
+// Less Common Primitibes
+
+/**
+ * BigInt
+ */
+const oneHundred: bigint = BigInt(100);
+// OR
+const anotherHundred: bigint = 100n;
+
+console.log(oneHundred);
+console.log(anotherHundred);
