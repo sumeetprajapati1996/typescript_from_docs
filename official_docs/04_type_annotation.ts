44400434f01238ab9c9210e70e1de7f4f28cebc3
Sumeet Prajapati <sumeetprajapati15@gmail.com>
2022-04-26 20:32:50 +0530

    Type Annotations in typescript

---
+++ official_docs/04_type_annotation.ts
@@ -0,0 +1,5 @@
+/**
+ * Type Annotation on Variables
+ */
+let myNameExp: string = "Sumeet"; // Explicitly secified annotation
+let myNameImp = "Sumeet"; // Typescript tries to automatically infer the type as string
