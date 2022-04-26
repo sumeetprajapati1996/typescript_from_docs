fccdc44ff3890e7bd04fbc8980edc4f6114979db
Sumeet Prajapati <sumeetprajapati15@gmail.com>
2022-04-27 02:11:08 +0530

    symbol in typescript

---
+++ official_docs/15_symbol.ts
@@ -0,0 +1,9 @@
+// Symbol
+
+/**
+ * Symbol()
+ */
+const firstName = Symbol("name");
+const secondName = Symbol("name");
+
+// console.log(firstName === secondName); // Will always be false because they both are unique
