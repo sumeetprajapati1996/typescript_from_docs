2a6117cb43cefc12e94eb8ef5df1d7235643c74d
Sumeet Prajapati <sumeetprajapati15@gmail.com>
2022-04-26 20:31:58 +0530

    Arrays in typescript

---
+++ official_docs/02_arrays.ts
@@ -0,0 +1,8 @@
+/**
+ * Arrays
+ */
+const numArray: number[] = [1, 2, 3];
+const numGenArray: Array<number> = [4, 5, 6]; // Generics
+
+const strArray: string[] = ["a", "b", "c"];
+const strGenArray: Array<string> = ["d", "e", "f"];
