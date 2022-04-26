e48c232a9ae158218aabc41fd230b6d1df0a200c
Sumeet Prajapati <sumeetprajapati15@gmail.com>
2022-04-26 20:31:08 +0530

    Primitive Types in typescript

---
+++ official_docs/01_primitive_types.ts
@@ -0,0 +1,10 @@
+/**
+ * Primitive Types
+ *
+ * using capitalized ('String', 'Number', 'Boolean')
+ * is legal but always use
+ * small case ('string', 'number', 'boolean')
+ */
+const message: string = "Hello World";
+const amount: number = 1000; // can be int/float doesnt matter
+const learning: boolean = true; // true|false
